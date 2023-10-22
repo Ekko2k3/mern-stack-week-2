603c76940ca792caab2708a17e55152730565f28
Nachiketh S Kotian <147600165+Ekko2k3@users.noreply.github.com>
2023-10-22 21:58:15 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import Cardlist from './Cardlist';
+import React from "react";
+// import Header from "./Components/Header";
+// import Body from "./Components/Body";
+// import Footer from "./Components/Footer";
 
-function App() {
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Navbar from "./Components/Navbar";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <Cardlist />
-    </div>
+    <>
+   <BrowserRouter>
+   <Navbar/>
+  <Routes>
+    <Route element={<Home/>} path="/Home" />
+    <Route element={<About/>} path="/about" />
+    <Route element={<Contact/>} path="/contact" />
+    <Route element={<Login/>} path="/login" />
+    <Route element={<Signup/>} path="/Signup" />
+  </Routes>
+</BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
---
+++ Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+function Home(){
+    
+    return(
+       <h3>Home</h3>
+    );
+};
+export default Home;
