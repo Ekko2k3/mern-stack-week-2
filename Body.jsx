996169afe66d78d10e6f569253524127e752b5b8
Nachiketh S Kotian <147600165+Ekko2k3@users.noreply.github.com>
2023-10-22 21:58:57 +0530

    Add files via upload

---
+++ Body.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+const Body=()=>{
+    const bodyStyle = {
+        backgroundColor: '#ffffff', /* Set your desired background color */
+        padding: '20px',
+    };
+    return(
+        <>
+    <hr/>
+    <center>
+    <h3>About me</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </p>
+    </center>
+    </>
+
+    );
+};
+
+export default Body;
---
+++ Footer.jsx
@@ -0,0 +1,18 @@
+import React,{Component}from 'react'
+class Footer extends Component{
+    render(){
+        return(
+        <>
+        <hr/>
+        <center>
+        <h3>Contact</h3>
+        <b>Mob:</b>
+        <p>789456730</p>
+        <b>Email:</b>
+        <p>123@gmail.com</p>
+        </center>
+        </>
+        );
+    }
+}
+export default Footer;
